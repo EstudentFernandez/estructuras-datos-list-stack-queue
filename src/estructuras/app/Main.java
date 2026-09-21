0c069b79ee97416c9489438f66aa0909460fa865
Julian Fernandez <jfernandezve@unal.edu.co>
2026-09-21 15:52:39 -0500

    Agregar punto de entrada Java

---
+++ src/estructuras/app/Main.java
@@ -0,0 +1,9 @@
+package estructuras.app;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Laboratorio de Estructuras de Datos");
+        System.out.println("Entorno Java funcionando correctamente.");
+    }
+}
